928cf15673320a33112ad6865dfc3c14af21dca5
rezabmic <rezabmic@k09-188.felk.cvut.cz>
2015-03-16 15:34:27 +0100

    merge

--- src/filemanager/find.c
+++ src/filemanager/find.c
@@ -661,9 +661,9 @@ find_parameters (char **start_dir, ssize_t * start_dir_len,
     content_use_cbox = check_new (y2++, x2, options.content_use, content_use_label);
     add_widget (find_dlg, content_use_cbox);
 
-    cbox_position = y2++;
+    int cbox_position = y2++;
 
-    only_directories_cbox = check_new (cbox_position, 3, options.only_directories, file_only_directories_label);
+    only_directories_cbox = check_new (cbox_position, x2, options.only_directories, file_only_directories_label);
     add_widget (find_dlg, only_directories_cbox);
 
 
